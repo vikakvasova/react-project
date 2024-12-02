a404d774e66e3a3cfb9f69c87fdd59f8aee23fe5
vikakvasova <vika.kvasova@yahoo.com>
2024-12-02 14:50:53 +0300

    add store, slice

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
-
+import  ReactDOM  from 'react-dom/client';
 import './App.css';
 import Button from './components/Button/Button';
 import Header from './components/Header/Header';
 import UIContext from './providers/UIContext';
 import MainPage from './pages/MainPage/MainPage';
+import { Provider } from 'react-redux';
+import store from './store/store';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 
 function App() {
   return (
     <>
-    <UIContext>
-    <MainPage></MainPage>
-    </UIContext>
+    <React.StrictMode>
+      <Provider store={store}>
+        <UIContext>
+          <MainPage></MainPage>
+        </UIContext>
+      </Provider>
+    </React.StrictMode>
+    
     </>
   );
 }
